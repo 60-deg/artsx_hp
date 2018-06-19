4746974c95e220d0f29763091685ca83b9939a57
60 <hatsumi.yamane.4609@gmail.com>
2018-06-20 03:17:00 +0900

    tel-child change sections to widgets

--- home/wp-content/themes/teletype-child/template-parts/section-widgets3.php
+++ home/wp-content/themes/teletype-child/template-parts/section-widgets3.php
@@ -5,20 +5,20 @@
  */
 ?>
 
-<?php if ( is_active_sidebar( 'home-widgets' ) ) { ?>
-<section id="home-widgets" class="text-left">
+<?php if ( is_active_sidebar( 'home-widgets3' ) ) { ?>
+<section id="home-widgets3" class="text-left">
 <?php
-	if ( get_theme_mod( 'section-widgets-title' ) || get_theme_mod( 'section-widgets-subtitle' ) ) {
+	if ( get_theme_mod( 'section-widgets3-title' ) || get_theme_mod( 'section-widgets3-subtitle' ) ) {
 ?>
 		<div class="section-title center">
-               			<h2><?php echo esc_html( get_theme_mod( 'section-widgets-title' ) ); ?></h2>
-               			<p><?php echo esc_html( get_theme_mod( 'section-widgets-subtitle' ) ); ?></p>
+               			<h2><?php echo esc_html( get_theme_mod( 'section-widgets3-title' ) ); ?></h2>
+               			<p><?php echo esc_html( get_theme_mod( 'section-widgets3-subtitle' ) ); ?></p>
 		</div>
 <?php
 	}
 ?>
 		<div class="row">
-			<?php dynamic_sidebar( 'home-widgets' ); ?>
+			<?php dynamic_sidebar( 'home-widgets3' ); ?>
 		</div><!-- .row -->
 
 </section>
