70aba70183c8790835d21ca5fd06f5b9ea6f6e87
60deg <hatsumi.yamane.4609@gmail.com>
2018-07-29 20:17:44 +0900

    test upcoming-events

--- home/wp-content/themes/teletype-child/template-parts/section-widgets.php
+++ home/wp-content/themes/teletype-child/template-parts/section-widgets.php
@@ -10,8 +10,15 @@
 <?php
 	if ( get_theme_mod( 'section-widgets-title' ) || get_theme_mod( 'section-widgets-subtitle' ) ) {
 ?>
-		<div class="section-title center">
-               			<h2><?php echo esc_html( get_theme_mod( 'section-widgets-title' ) ); ?></h2>
+		<div class="section-title center"><?php 
+			if ( get_theme_mod( 'section-widgets-title' )=="Upcoming Events" ){ ?>
+				<a href="http://arts-x.sakura.ne.jp/upcoming-events/"><h2> <?php echo esc_html( get_theme_mod( 'section-widgets-title' ) ) ?> </h2></a>;
+			<?php }else{ ?>
+				<h2><?php esc_html( get_theme_mod( 'section-widgets-title' ) ) ?></h2>;
+			<?php 
+				} 
+			?>
+               			<!-- <h2><?php //echo esc_html( get_theme_mod( 'section-widgets-title' ) ); ?></h2> -->
                			<p><?php echo esc_html( get_theme_mod( 'section-widgets-subtitle' ) ); ?></p>
 		</div>
 <?php
