25884513d89313b6bd69a66a1e19609f06dc7d46
60deg <hatsumi.yamane.4609@gmail.com>
2018-07-29 15:48:30 +0900

    get_the_category

--- home/wp-content/themes/teletype-child/template-parts/headline.php
+++ home/wp-content/themes/teletype-child/template-parts/headline.php
@@ -34,7 +34,12 @@
 	<section id="headline" class="text-center"<?php teletype_header_bg(); ?>>
         <div class="head-content">
 									<!-- single page title -->
-            			<h1><?php get_the_category($post->ID)[0]->name; ?></h1><hr>
+            			<h1>
+										<?php 
+											$category = get_the_category(); 
+											echo $category[0]->name; 
+										?>
+									</h1><hr>
             			<?php if ( has_excerpt() ) : ?>
 				<?php the_excerpt(); ?>
 			<?php endif; ?>
